d55ab47f0d99f944baf82b0156145862fcbb8567
Branislav Nebus <branislav.nebus@kosickaakademia.sk>
2021-02-18 19:12:55 +0100

    added the classes main and car

---
+++ src/sk/kosickaakademia/nebus/branches/Auto.java
@@ -0,0 +1,9 @@
+package sk.kosickaakademia.nebus.branches;
+
+public class Auto {
+    String name;
+
+    public Auto(String name){
+        this.name = name;
+    }
+}
---
+++ src/sk/kosickaakademia/nebus/branches/Main.java
@@ -0,0 +1,7 @@
+package sk.kosickaakademia.nebus.branches;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Toto bude v hlavnej vetve");
+    }
+}
