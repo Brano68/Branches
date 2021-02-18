a691d9c3e89d43d11def930a6746a605b42f2390
kosickaakademia <72263463+kosickaakademia@users.noreply.github.com>
2021-02-18 20:14:13 +0100

    Update Auto.java
    changed

--- src/sk/kosickaakademia/nebus/branches/Auto.java
+++ src/sk/kosickaakademia/nebus/branches/Auto.java
@@ -3,10 +3,16 @@ package sk.kosickaakademia.nebus.branches;
 public class Auto {
     String name;
 
-    public Auto(String name){
-        this.name = name;
+    public Auto(String nameYY){
+        // this is my comment
+        this.name = nameYY;
     }
 
+    public int getInt(){
+        return -5;
+    }
+    
+    // comment
     public String getName() {
         return name;
     }
