e29a4e9202766a3ab9b0ba9959eab9e349f8e76f
Branislav Nebus <branislav.nebus@kosickaakademia.sk>
2021-02-18 19:16:41 +0100

    added a new object and getter

--- src/sk/kosickaakademia/nebus/branches/Auto.java
+++ src/sk/kosickaakademia/nebus/branches/Auto.java
@@ -6,4 +6,8 @@ public class Auto {
     public Auto(String name){
         this.name = name;
     }
+
+    public String getName() {
+        return name;
+    }
 }
--- src/sk/kosickaakademia/nebus/branches/Main.java
+++ src/sk/kosickaakademia/nebus/branches/Main.java
@@ -3,5 +3,7 @@ package sk.kosickaakademia.nebus.branches;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Toto bude v hlavnej vetve");
+
+        Auto auto = new Auto("Citroen");
     }
 }
